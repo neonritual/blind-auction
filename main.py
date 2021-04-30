6a3159d47de3b033e80bfec0d9214acb54d86378
V. S <>
2021-04-30 15:15:18 +0000

    added teacher's version

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
+from art import logo
+print(logo)
 
 x = True
 bid_dictionary = {}
 top_score = 0
 while x is True:
-  name = input("What's your name?\n")
-  bid = input("What's your bid?\n")
-  bid_dictionary[name] = bid
-  print(bid_dictionary)
-  more_players = input("Are there any other players?\n")
-  if more_players == "no":
-    clear()
-    x = False
-    maximum = max(bid_dictionary, key=bid_dictionary.get)  # Just use 'min' instead of 'max' for minimum.
-    print(f"The top bidder is {maximum}, with a big of ${bid_dictionary[maximum]})
-  else: 
-    clear()
-    continue
+    name = input("What's your name?\n")
+    bid = int(input("What's your bid?\n$"))
+    bid_dictionary[name] = bid
+    print(bid_dictionary)
+    more_players = input("Are there any other players?\n")
+    if more_players == "no":
+        clear()
+        x = False
+        for player in bid_dictionary:
+          bid_amount = bid_dictionary[player]
+          if bid_amount > top_score:
+            top_score = bid_amount
+            winner = player
+        print("The winning bid is " + str(winner) + " with $" + str(top_score) + ". Congrats!")
+        
+    else:
+        clear()
+        continue
 
-    
+#From here is the teacher's solution, for comparison and study purposes笑
 
-# take_bids()
-# bid_dictionary[name] = bid
+# bids = {}
+# bidding_finished = False
 
-# print(bid_dictionary)
+# def find_highest_bidder(bidding_record):
+#   highest_bid = 0
+#   #bidding_record = ("Angela": 123, "James": 321)
+#   for bidder in bidding_record:
+#     bid_amount = bidding_record[bidder]
+#     if bid_amount > highest_bid:
+#       highest_bid = bid_amount
+#       winner = bidder
+#   print(f"The winner is {winner} with a bid of ${highest_bid}")
+  
+# while not bidding_finished:
+#   name = input("What is your name?: ")
+#   price = int(input("What is your bid?: $"))
+#   bids[names] = price
+#   should_continue = input ("Are there any other bidders? Type 'yes' or 'no'.")
+#   if should_continue == "no":
+#     bidding_finished = True
+#     find_highest_bidder(bids)
+#   elif should_continue == "yes":
+#     clear()
